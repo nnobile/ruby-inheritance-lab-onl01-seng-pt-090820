861c78cc440598104952dce72ef9e464a0ec654e
Nick Nobile <nickcnobile@gmail.com>
2020-10-12 01:27:17 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,17 @@
-class Student 
+class Student < User
+
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(knowledge = "Ruby framework Rails gem bundle update")
+    @knowledge << knowledge
+  end
+
+  def knowledge
+    @knowledge
+  end
+
+
 
 end
